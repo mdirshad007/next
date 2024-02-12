f4a91952903dea931a83e9ffe3dcb610ac5d58f6
mdirshad007 <md.irshad8447@gmail.com>
2024-02-12 22:41:36 +0530

    fix cpitalize case

--- app/page.js
+++ app/page.js
@@ -20,7 +20,7 @@ export default function Home() {
     let words = text.split(" ");
     // Capitalize the first letter of each word
     for (let i = 0; i < words.length; i++) {
-      words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+      words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
     }
     // Join the words back into a sentence
     let capitalizedText = words.join(" ");
